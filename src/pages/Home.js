ba37820a5f46961a8c978d0f447ee55597dc3976
Kyle Moorhead <Moorhead212@gmail.com>
2023-09-14 09:43:24 -0400

    Completed server functions
    Completed the needed server functions to get and send info from the server to the front-end.
    Completed connection from server to front-end

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,46 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
-    return (<h1>Home!</h1>)
-  };
-  
-  export default Home;
-  
+    const [message, setMessage] = useState("");
+    const [nameInput, setNameInput] = useState(""); // New state for the name input
+
+    // Contacts the backend to retrieve the information
+    useEffect(() => {
+        fetch("http://localhost:8000")
+            .then((res) => res.json())
+            .then((data) => setMessage(data));
+    }, []);
+
+    const handleSubmit = () => {
+        // Send the name input to the server
+        fetch("http://localhost:8000/submit-name", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name: nameInput }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+            });
+
+        // Empty the name input value for re-use
+        setNameInput("");
+    };
+
+    return (
+        <div className="App">
+            <h1>{message}</h1>
+            <input
+                type="text"
+                placeholder="Enter your name"
+                value={nameInput}
+                onChange={(e) => setNameInput(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Submit!</button>
+        </div>
+    );
+};
+
+export default Home;
--- src/server/server.js
+++ src/server/server.js
@@ -1,32 +1,49 @@
 require('dotenv').config()
 
 // If you use "npm run dev" while inside the server folder it should run the script with nodemon which will update the server automatically on file saves 
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
 
-// creates express app 
-const express = require('express')
-const cors = require('cors')
-
-const app = express()
+const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-// Just a holder file for the names list since we're not using a database for it
-const names = require('./names.json')
+// Imports the names json for name storage
+const names = require('./names.json');
 
-
-// listen for requests
+// Logs the port # the server is on
 app.listen(process.env.PORT, () => {
-    console.log('listening on port ' + process.env.PORT)
-})
+    console.log('listening on port ' + process.env.PORT);
+});
 
-// routes
+// Gives the front end the index information
 app.get('/', (req, res, next) => {
-    // This should probably get the index page but I'm not sure how to do that
-})
+    res.status(200).json("Hello! Enter your name: ");
+});
 
-// this route will get the names from the json file
+// Gives the front end the list of names to be used on the /names page
 app.get('/names', (req, res, next) => {
-    // This should get the names page but I don't know how to do that either
-    res.send(names)
-})
+    // Send the list of names as a JSON array
+    const namesList = names.people.map(person => person.name);
+    res.status(200).json(namesList);
+});
+
+// Functions to submit a name to the json file for storage
+app.post('/submit-name', (req, res) => {
+    const { name } = req.body;
+    
+    // If nothing is entered, it will not push empty name to the json
+    if (!name) {
+        return res.status(400).json({ error: "Name is required" });
+    }
+
+    // Add the new name to the json
+    names.people.push({ name });
+
+    // Write the information to the json
+    fs.writeFileSync('./names.json', JSON.stringify(names, null, 2));
+
+    res.status(201).json({ message: "Name submitted successfully" });
+});
