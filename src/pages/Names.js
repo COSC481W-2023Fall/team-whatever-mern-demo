435f24a97d96f86aac6d6e20242331e40a3bf87d
doormatt65 <mbraun3@emich.edu>
2023-09-14 20:17:19 -0400

    with no names in the list it was just saying 'hello,  '. Fixed so it asks for name

--- src/pages/Names.js
+++ src/pages/Names.js
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react";
 
 const Names = () => {
-    const [names, setNames] = useState([]);
-    
-    // Calls to the back end to get the names information from the json
-    useEffect(() => {
-        fetch("http://localhost:8000/names")
-            .then((res) => res.json())
-            .then((data) => setNames(data));
-    }, []);
-    
-    return (
-        <div>
-            <h1>Names</h1>
-            <ul>
-                {/* For loop for each name in the list of names */}
-                {names.map((name) => (
-                    <li>{name}</li>
-                ))}
-            </ul>
-        </div>
-    );
+  const [names, setNames] = useState([]);
+
+  // Calls to the back end to get the names information from the json
+  useEffect(() => {
+    fetch("http://localhost:8000/names")
+      .then((res) => res.json())
+      .then((data) => setNames(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Names</h1>
+      <ul>
+        {/* For loop for each name in the list of names */}
+        {names
+          .filter((name) => name !== "please enter your name")
+          .map((name) => (
+            <li>{name}</li>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Names;
