476d09e6ae151149637e2a3d99d688798203b9a1
khammerm <khammermeister44@gmail.com>
2023-09-21 19:59:28 -0400

    Testing database connection

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const mongoose = require('mongoose')
 const PORT = 3030;
 
 const app = express();
