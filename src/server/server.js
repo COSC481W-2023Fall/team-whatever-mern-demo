c1564b448291367e21cf5b9d98bd78aed47735d3
khammerm <khammermeister44@gmail.com>
2023-09-24 16:49:03 -0400

    trying .mjs again

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
-import db from "./conn.js";
+import db from "./conn.mjs";
 import { ObjectId } from "mongodb";
 const PORT = 3030;
 
