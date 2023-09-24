9e042790d5371030c1a6d19048848a75ccd22310
Kyle Moorhead <Moorhead212@gmail.com>
2023-09-24 14:13:53 -0400

    .mjs to .js

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
-import db from "../db/conn.mjs";
+import db from "../db/conn.js";
 import { ObjectId } from "mongodb";
 const PORT = 3030;
 
