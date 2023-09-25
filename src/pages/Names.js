26a45c96b92db90efea3153aa896a6f691e2e7c5
doormatt65 <mbraun3@emich.edu>
2023-09-24 23:03:29 -0400

    names page fix pt 3?

--- src/pages/Names.js
+++ src/pages/Names.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
-
-
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
 // const UserSchema = new mongoose.Schema({
 //     name: String
@@ -17,23 +15,22 @@ const Names = () => {
   // Calls to the back end to get the names information from the json
   useEffect(() => {
     fetch("https://demo-webserver.onrender.com/names")
-      .then((res) => res.json())
+      .then((res) => res.json().stringify())
       .then((data) => setNames(data));
   }, []);
-
 
   return (
     <Card>
       <Card.Body>
         <Card.Title>Names</Card.Title>
         <ListGroup>
-        {/* For loop for each name in the list of names */}
-        {names
-          .filter((name) => name !== "please enter your name")
-          .map((name) => (
-            <ListGroup.Item>{name}</ListGroup.Item>
-          ))}
-      </ListGroup>
+          {/* For loop for each name in the list of names */}
+          {names
+            .filter((name) => name !== "please enter your name")
+            .map((name) => (
+              <ListGroup.Item>{name}</ListGroup.Item>
+            ))}
+        </ListGroup>
       </Card.Body>
     </Card>
   );
